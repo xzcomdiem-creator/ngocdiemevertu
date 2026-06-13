5deb5b1a0994d3dd522f36af4fd051f9c7321d5d
xzcomdiem-creator <xzcomdiem@gmail.com>
2026-06-13 20:59:20 +0700

    Enhance service worker with activation and fetch logic
    Refactor service worker to include activate event and improve fetch handling.

--- sw.js
+++ sw.js
@@ -1,9 +1,22 @@
 const CACHE = 'timeflow-v1';
-const FILES = ['./', './index.html', './manifest.json'];
+const FILES = ['./index.html', './manifest.json'];
 
-self.addEventListener('install', e =>
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)))
-);
-self.addEventListener('fetch', e =>
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
-);
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
